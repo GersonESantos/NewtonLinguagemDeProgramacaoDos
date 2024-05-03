f7a90fb471fcb3a5f8ac1ae6811160c0ed1f5cda
Gerson Santos <gersones@outlook.com>
2024-05-03 14:58:14 -0300

    a

---
+++ Exemplo01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>;
+int main() {
+    int num = 0;
+    int cubo = 0 ;
+    printf("Cubo de um Numero\n\n");
+    printf("Digite um Numero :");
+    scanf("%d", &num);
+    cubo = num*num*num;
+    printf("\n Cubo de %d = %d\n" ,num , cubo);
+    return 0;
+}
