ef38e11ec6d5b54db3b1f843cc57103646acceed
Gerson Santos <gersones@outlook.com>
2024-05-04 10:04:44 -0300

    a

--- Exemplo09.c
+++ Exemplo09.c
@@ -4,14 +4,14 @@
 int main() {
     int *pt, valor1, valor2;
     valor1 = 15;
-    // inicializando o ponteiro pt com o endereço de valor1
+    // inicializando o ponteiro pt com o endere�o de valor1
     pt = &valor1;
     // valor2 recebe o conteúdo de pt, ou seja 15
     valor2 = *pt;
     // o ponteiro pt modifica o valor da variável valor1,
     // para a qual ele aponta. Ou seja, valor1=12;
     *pt = 12;
-    // valor2 recebe o conteúdo da variável para a qual pt aponta, ou seja, valor2=12.
+    // valor2 recebe o conteudo da variavel para a qual pt aponta, ou seja, valor2=12.
     valor2 = *pt;
     printf("%i %i \n\n\n", valor1, valor2);
     system("PAUSE");
