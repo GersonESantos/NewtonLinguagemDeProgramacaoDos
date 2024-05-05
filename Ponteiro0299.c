5b4db00f5a6382f03e042b870b9a7665d1ab95f3
Gerson Santos <gersones@outlook.com>
2024-05-05 15:13:19 -0300

    a

--- Ponteiro0299.c
+++ Ponteiro0299.c
@@ -1,7 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <locale.h>
 
-int main(){
-	int x;
-    printf("Valor inicial da idade: %d.\n", x);
+int main() {
+    int *ptrx, **pptrx, x;
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    
+    x = 0;
+    
+
+    printf("\nValor de x = %d\n", x);
+    printf("Endereço de x: %x\n", &x);
+
+    ptrx  = &x;
+    pptrx = &ptrx;
+    printf("\nValor de x = %d", x);
+     printf("\nEndereço apontado por ptrx: %x", ptrx);
+      printf("\nValor da variavel x que esta sendo apontada por ptrx: %x", *ptrx);
+    printf("\nEndereço de memoria de variavel ptrx apontado por ptrx: %x", &ptrx);
     return 0;
 }
