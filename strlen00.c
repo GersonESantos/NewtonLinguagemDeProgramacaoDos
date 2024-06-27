4800e4b9181c0020d75d4023d9dcd9471b97c1ba
Gerson Santos <gersones@outlook.com>
2024-06-27 13:33:51 -0300

    a

---
+++ strlen00.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char texto[21];
+    printf("Digite a palavra: ");
+        gets(texto);
+        printf("%i", strlen(texto));    
+    }
+    
